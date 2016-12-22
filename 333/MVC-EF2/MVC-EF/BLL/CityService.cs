ad022b11d57e84b3f187b24660ae8006936a383b
PiNuoshan <1012688954@qq.com>
2016-12-22 10:18:14 +0800

    修改

---
+++ 333/MVC-EF2/MVC-EF/BLL/CityService.cs
@@ -0,0 +1,28 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+
+namespace BLL
+{
+    public class CityService
+    {
+        public static List<CityServiceInfo> GetAllInfo()
+        {
+            using (dbContext db=new dbContext())
+            {
+                return db.ServiceInfos.ToList();
+            }
+        }
+        public static List<CityServiceInfo> GetUsers()
+        {
+            using (var db = new dbContext())
+            {
+                return db.ServiceInfos.OrderBy(m => m.Id).ToList();
+            }
+        }
+    }
+}
